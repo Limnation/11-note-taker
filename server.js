73e6dfee63a6579d642df49cf38c9711dd178d01
Scott Erwin <scottallenerwin@gmail.com>
2021-04-30 18:07:18 -0400

    added path require as a way of working with directories and file paths

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ const fs = require("fs");
 
 // Requires the DB json file
 let db = require("./db/db.json");
+
+// Requires path (provides a way of working with directories and file paths)
+var path = require("path");
