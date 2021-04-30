1d8e80f415f4b492d63b2e346cb62cca9d42a631
Scott Erwin <scottallenerwin@gmail.com>
2021-04-30 18:11:55 -0400

    added static port

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 // Requires express
 const express = require("express");
 
@@ -9,3 +10,9 @@ let db = require("./db/db.json");
 
 // Requires path (provides a way of working with directories and file paths)
 var path = require("path");
+
+// Express App
+const app = express();
+
+// Port
+const PORT = 8080;
