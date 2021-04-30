309b656636547af966a29204af48eac4e0ca4c0c
Scott Erwin <scottallenerwin@gmail.com>
2021-04-30 18:03:58 -0400

    added requires to server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// Requires express
+const express = require("express");
+
+// Requires FS (file ststem)
+const fs = require("fs");
+
+// Requires the DB json file
+let db = require("./db/db.json");
